2d002bb7eca8c01a7daa5a1901433f1eebe4b261
Araz Abishov <arabisho@microsoft.com>
2020-02-07 09:57:18 +0100

    Initial implementation of the test app for android

---
+++ android/app/src/androidTest/java/com/sample/ExampleInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package com.sample
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    
+    @Test
+    fun useAppContext() {        
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.sample", appContext.packageName)
+    }
+}
