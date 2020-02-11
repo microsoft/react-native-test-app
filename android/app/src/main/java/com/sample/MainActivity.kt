9b6ea3e9d16220f3e61ad4979235e6465c3ede83
Araz Abishov <arabishov@gmail.com>
2020-02-11 10:38:07 +0100

    Initial implementation of the test app for android (#6)
    * Initial implementation of the test app for android
    
    * Applying suggestions from the code review.
    
    * Removing redundant package.json and yarn.lock files
    
    * Avoid depending on specific version of rn
    
    * Dynamically locating node modules folder
    
    * Dynamically find the application folder
    
    * Use clear text for loading bundle
    
    * Updates application name
    
    * New line file endings
    
    * Simplifying plugin implementation
    
    * Removing redundant argument

---
+++ android/app/src/main/java/com/sample/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.sample
+
+import android.os.Bundle
+import android.view.KeyEvent
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.react.PackageList
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactRootView
+import com.facebook.react.TestAppPackageList
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.soloader.SoLoader
+
+class MainActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
+    private lateinit var reactRootView: ReactRootView
+    private lateinit var reactInstanceManager: ReactInstanceManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        SoLoader.init(this, false)
+
+        reactRootView = ReactRootView(this)
+        setContentView(reactRootView)
+
+        reactInstanceManager = ReactInstanceManager.builder()
+            .setInitialLifecycleState(LifecycleState.BEFORE_RESUME)
+            .addPackages(PackageList(application).packages)
+            .addPackages(TestAppPackageList().packages)
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setCurrentActivity(this)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModulePath("index")
+            .setApplication(application)
+            .build()
+
+        reactRootView.startReactApplication(
+            reactInstanceManager, "TestComponent", null
+        )
+    }
+
+    override fun invokeDefaultOnBackPressed() {
+        onBackPressed()
+    }
+
+    override fun onBackPressed() {
+        reactInstanceManager.onBackPressed()
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_MENU) {
+            reactInstanceManager.showDevOptionsDialog()
+            return true
+        }
+        return super.onKeyUp(keyCode, event)
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+
+        reactInstanceManager.onHostPause(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        reactInstanceManager.onHostResume(this, this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        reactInstanceManager.onHostDestroy(this)
+        reactRootView.unmountReactApplication()
+    }
+}
---
+++ android/app/src/test/java/com/sample/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package com.sample
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
