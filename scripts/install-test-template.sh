21bfea8c1902f2e472ad15a5fd7a32589492810c
Tommy Nguyen <tonguye@microsoft.com>
2020-03-20 11:26:41 +0100

    Remove node_modules after generating the app

--- scripts/install-test-template.sh
+++ scripts/install-test-template.sh
@@ -2,7 +2,9 @@
 set -eo pipefail
 
 platform=${1:-All}
+yarn
 yarn plop --dest template-example TemplateExample $platform
+rm -rf node_modules
 pushd template-example 1> /dev/null
 sed -e 's/"\(react-native-test-app\)": "[0-9][.0-9]*"/"\1": "..\/"/' -i '' package.json
 yarn
