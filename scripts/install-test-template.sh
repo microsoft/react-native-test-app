7b6bb5f961daa44e01aa3c22dc7997ceb48ba227
Tommy Nguyen <tonguye@microsoft.com>
2020-03-20 11:18:13 +0100

    Install react-native first

--- scripts/install-test-template.sh
+++ scripts/install-test-template.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 platform=${1:-All}
-npx plop --dest template-example TemplateExample $platform
+yarn plop --dest template-example TemplateExample $platform
 pushd template-example 1> /dev/null
 sed -e 's/"\(react-native-test-app\)": "[0-9][.0-9]*"/"\1": "..\/"/' -i '' package.json
 yarn
