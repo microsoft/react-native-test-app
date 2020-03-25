487114bbeeaae0277c184cdc8cf77c1b7eabe56b
Araz Abishov <arabisho@microsoft.com>
2020-03-25 22:59:55 +0100

    Move file level var to the method def

--- android/app/src/main/java/com/sample/react/ReactBundleNameProvider.kt
+++ android/app/src/main/java/com/sample/react/ReactBundleNameProvider.kt
@@ -4,20 +4,20 @@ import android.content.Context
 import javax.inject.Inject
 import javax.inject.Singleton
 
-private val possibleEntryFiles = listOf(
-    "index.android",
-    "main.android",
-    "index.mobile",
-    "main.mobile",
-    "index.native",
-    "main.native",
-    "index",
-    "main"
-).map { "$it.jsbundle" }
-
 @Singleton
 class ReactBundleNameProvider @Inject constructor(private val context: Context) {
     val bundleName: String? by lazy {
+        val possibleEntryFiles = listOf(
+            "index.android",
+            "main.android",
+            "index.mobile",
+            "main.mobile",
+            "index.native",
+            "main.native",
+            "index",
+            "main"
+        ).map { "$it.jsbundle" }
+
         context.resources.assets.list("")
             ?.firstOrNull { possibleEntryFiles.contains(it) }
     }
