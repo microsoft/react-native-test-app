c56195c8d5501405d75f1fb8295395a4934dde1f
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2020-04-14 15:18:45 +0200

    Android: Add menu for changing JS bundle source (#63)

--- android/app/src/main/java/com/sample/MainActivity.kt
+++ android/app/src/main/java/com/sample/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.facebook.react.ReactActivity
+import com.google.android.material.appbar.MaterialToolbar
 import com.sample.component.ComponentActivity
 import com.sample.component.ComponentListAdapter
 import com.sample.component.ComponentViewModel
 import com.sample.manifest.ManifestProvider
+import com.sample.react.TestAppReactNativeHost
 import dagger.android.AndroidInjection
 import javax.inject.Inject
 
@@ -20,9 +22,9 @@ class MainActivity : ReactActivity() {
 
     private val listener = { component: ComponentViewModel ->
         startActivity(
-            ComponentActivity.newIntent(
-                this, component.name, component.displayName
-            )
+                ComponentActivity.newIntent(
+                        this, component.name, component.displayName
+                )
         )
     }
 
@@ -32,21 +34,55 @@ class MainActivity : ReactActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val reactNativeHost = reactNativeHost
+        if (reactNativeHost is TestAppReactNativeHost) {
+            reactNativeHost.currentActivity = this
+        }
+
         val manifest = manifestProvider.manifest
-            ?: throw IllegalStateException("app.json is not provided or TestApp is misconfigured")
+                ?: throw IllegalStateException("app.json is not provided or TestApp is misconfigured")
+        findViewById<MaterialToolbar>(R.id.top_app_bar).apply {
+            title = manifest.displayName
+            setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.load_embedded_js_bundle -> {
+                        reload(this, true)
+                        true
+                    }
+                    R.id.load_from_dev_server -> {
+                        reload(this, false)
+                        true
+                    }
+                    R.id.show_dev_options -> {
+                        reactInstanceManager.devSupportManager.showDevOptionsDialog()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+
         val components = manifest.components.map {
             ComponentViewModel(it.key, it.value.displayName ?: it.key)
         }
-
-        supportActionBar?.title = manifest.displayName
-
         findViewById<RecyclerView>(R.id.recyclerview).apply {
             layoutManager = LinearLayoutManager(context)
             adapter = ComponentListAdapter(
-                LayoutInflater.from(context), components, listener
+                    LayoutInflater.from(context), components, listener
             )
 
             addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        }
+    }
+
+    private fun reload(toolbar: MaterialToolbar, useEmbeddedBundle: Boolean) {
+        val reactNativeHost = reactNativeHost
+        if (reactNativeHost is TestAppReactNativeHost) {
+            reactNativeHost.reload(useEmbeddedBundle)
+        }
+
+        toolbar.menu.findItem(R.id.show_dev_options)?.apply {
+            isEnabled = !useEmbeddedBundle
         }
     }
 }
--- android/app/src/main/java/com/sample/react/TestAppReactNativeHost.kt
+++ android/app/src/main/java/com/sample/react/TestAppReactNativeHost.kt
@@ -1,22 +1,63 @@
 package com.sample.react
 
 import android.app.Application
-import com.facebook.react.PackageList
-import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
+import com.facebook.react.*
+import com.facebook.react.bridge.ReactMarker
+import com.facebook.react.bridge.ReactMarkerConstants
+import com.facebook.react.common.LifecycleState
+import com.facebook.soloader.SoLoader
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class TestAppReactNativeHost @Inject constructor(
-    application: Application,
-    private val reactBundleNameProvider: ReactBundleNameProvider
+        application: Application,
+        private val reactBundleNameProvider: ReactBundleNameProvider
 ) : ReactNativeHost(application) {
 
-    override fun getBundleAssetName() =
-        reactBundleNameProvider.bundleName ?: super.getBundleAssetName()
+    var currentActivity: ReactActivity? = null
+    private var useEmbeddedBundle: Boolean = true
 
-    override fun getUseDeveloperSupport() = reactBundleNameProvider.bundleName == null
+    fun reload(useEmbeddedBundle: Boolean) {
+        if (hasInstance()) {
+            if (!useEmbeddedBundle && useEmbeddedBundle == this.useEmbeddedBundle) {
+                reactInstanceManager.devSupportManager.handleReloadJS()
+                return
+            }
+            clear()
+        } else {
+            SoLoader.init(application, false)
+        }
+
+        this.useEmbeddedBundle = useEmbeddedBundle
+        reactInstanceManager.createReactContextInBackground()
+    }
+
+    override fun createReactInstanceManager(): ReactInstanceManager {
+        ReactMarker.logMarker(ReactMarkerConstants.BUILD_REACT_INSTANCE_MANAGER_START)
+        val reactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(application)
+                .setCurrentActivity(currentActivity)
+                .setJavaScriptExecutorFactory(javaScriptExecutorFactory)
+                .setBundleAssetName(bundleAssetName)
+                .setJSMainModulePath(jsMainModuleName)
+                .addPackages(packages)
+                .setUseDeveloperSupport(useDeveloperSupport && !useEmbeddedBundle)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .setUIImplementationProvider(uiImplementationProvider)
+                .setRedBoxHandler(redBoxHandler)
+                .setJSIModulesPackage(jsiModulePackage)
+                .build()
+        ReactMarker.logMarker(ReactMarkerConstants.BUILD_REACT_INSTANCE_MANAGER_END)
+        return reactInstanceManager
+    }
+
+    override fun getJSMainModuleName(): String = "index"
+
+    override fun getBundleAssetName(): String? =
+            if (useEmbeddedBundle) reactBundleNameProvider.bundleName else null
+
+    override fun getUseDeveloperSupport() = true
 
     override fun getPackages(): List<ReactPackage> = PackageList(application).packages
 }
