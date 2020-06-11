d4cbb7a1db8144c3f2bf3dad8bc0e216f4bf0a5f
Tommy Nguyen <tonguye@microsoft.com>
2020-06-11 10:29:00 +0200

    Add preliminary support for 0.62
    Android and iOS on React Native 0.62 both work after this commit. We're
    still missing a way for people to enable Flipper (#112) and we're
    waiting on RN for macOS to release 0.62.
    
    Resolves #110

---
+++ ios/use_react_native-0.62.rb
@@ -0,0 +1,42 @@
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# rubocop:disable Layout/LineLength
+
+def include_react_native!(react_native, _target_platform, _project_root)
+  pod 'FBLazyVector', :path => "#{react_native}/Libraries/FBLazyVector"
+  pod 'FBReactNativeSpec', :path => "#{react_native}/Libraries/FBReactNativeSpec"
+  pod 'RCTRequired', :path => "#{react_native}/Libraries/RCTRequired"
+  pod 'RCTTypeSafety', :path => "#{react_native}/Libraries/TypeSafety"
+  pod 'React', :path => "#{react_native}/"
+  pod 'React-Core', :path => "#{react_native}/", :inhibit_warnings => true
+  pod 'React-CoreModules', :path => "#{react_native}/React/CoreModules"
+  pod 'React-Core/DevSupport', :path => "#{react_native}/"
+  pod 'React-RCTActionSheet', :path => "#{react_native}/Libraries/ActionSheetIOS"
+  pod 'React-RCTAnimation', :path => "#{react_native}/Libraries/NativeAnimation"
+  pod 'React-RCTBlob', :path => "#{react_native}/Libraries/Blob"
+  pod 'React-RCTImage', :path => "#{react_native}/Libraries/Image"
+  pod 'React-RCTLinking', :path => "#{react_native}/Libraries/LinkingIOS"
+  pod 'React-RCTNetwork', :path => "#{react_native}/Libraries/Network"
+  pod 'React-RCTSettings', :path => "#{react_native}/Libraries/Settings"
+  pod 'React-RCTText', :path => "#{react_native}/Libraries/Text", :inhibit_warnings => true
+  pod 'React-RCTVibration', :path => "#{react_native}/Libraries/Vibration"
+  pod 'React-Core/RCTWebSocket', :path => "#{react_native}/"
+
+  pod 'React-cxxreact', :path => "#{react_native}/ReactCommon/cxxreact", :inhibit_warnings => true
+  pod 'React-jsi', :path => "#{react_native}/ReactCommon/jsi"
+  pod 'React-jsiexecutor', :path => "#{react_native}/ReactCommon/jsiexecutor"
+  pod 'React-jsinspector', :path => "#{react_native}/ReactCommon/jsinspector"
+  pod 'ReactCommon/callinvoker', :path => "#{react_native}/ReactCommon"
+  pod 'ReactCommon/turbomodule/core', :path => "#{react_native}/ReactCommon"
+  pod 'Yoga', :path => "#{react_native}/ReactCommon/yoga", :modular_headers => true
+
+  pod 'DoubleConversion', :podspec => "#{react_native}/third-party-podspecs/DoubleConversion.podspec"
+  pod 'glog', :podspec => "#{react_native}/third-party-podspecs/glog.podspec"
+  pod 'Folly', :podspec => "#{react_native}/third-party-podspecs/Folly.podspec"
+end
+
+# rubocop:enable Layout/LineLength
