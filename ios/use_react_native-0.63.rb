07c93eb6294ab3eb80cb597c9ef2cb34ddb10385
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2020-06-24 11:03:17 +0200

    Actually use `File.join`
    Co-authored-by: Eloy Durán <eloy.de.enige@gmail.com>

--- ios/use_react_native-0.63.rb
+++ ios/use_react_native-0.63.rb
@@ -6,7 +6,7 @@
 #
 
 def include_react_native!(react_native:, target_platform:, project_root:, flipper_versions:)
-  require_relative "#{File.join(project_root, react_native)}/scripts/react_native_pods"
+  require_relative File.join(project_root, react_native, 'scripts', 'react_native_pods')
   use_flipper!(flipper_versions) if target_platform == :ios && flipper_versions
   use_react_native!(:path => react_native)
 end
