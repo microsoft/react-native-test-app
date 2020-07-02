2102ee30d1bb73ba49bbbe7a4606d402dd25f6ea
Tommy Nguyen <tn0502@gmail.com>
2020-07-02 23:08:21 +0200

    fix(Apple): Complex `initialProperties` may fail the app

--- example/ios/ExampleTests/ManifestTests.swift
+++ example/ios/ExampleTests/ManifestTests.swift
@@ -35,7 +35,24 @@ class ManifestTests: XCTestCase {
             ]
         )
 
-        guard let data = try? JSONEncoder().encode(expected),
+        let json = """
+            {
+                "name": "\(expected.name)",
+                "displayName": "\(expected.displayName)",
+                "components": [
+                    {
+                        "appKey": "\(expected.components[0].appKey)",
+                        "initialProperties": { "key": "value" }
+                    },
+                    {
+                        "appKey": "\(expected.components[1].appKey)",
+                        "displayName": "\(expected.components[1].displayName!)"
+                    }
+                ]
+            }
+        """
+
+        guard let data = json.data(using: .utf8),
               let manifest = Manifest.from(data: data) else {
             XCTFail("Failed to read manifest")
             return
@@ -49,15 +66,90 @@ class ManifestTests: XCTestCase {
                        expected.components[0].appKey)
         XCTAssertEqual(manifest.components[0].displayName,
                        expected.components[0].displayName)
-        XCTAssertEqual(manifest.components[0].initialProperties!["key"]!,
-                       expected.components[0].initialProperties!["key"]!)
+        XCTAssertEqual(manifest.components[0].initialProperties!["key"] as! String,
+                       expected.components[0].initialProperties!["key"] as! String)
 
         XCTAssertEqual(manifest.components[1].appKey,
                        expected.components[1].appKey)
         XCTAssertEqual(manifest.components[1].displayName,
                        expected.components[1].displayName)
-        XCTAssertEqual(manifest.components[1].initialProperties,
-                       expected.components[1].initialProperties)
+        XCTAssertNil(manifest.components[1].initialProperties)
+        XCTAssertNil(expected.components[1].initialProperties)
     }
 
+    func testComplexInitialProperties() {
+        let json = """
+            {
+                "name": "Name",
+                "displayName": "Display Name",
+                "components": [
+                    {
+                        "appKey": "AppKey",
+                        "initialProperties": {
+                            "boolean": true,
+                            "number": 9000,
+                            "string": "string",
+                            "array": [
+                                null,
+                                true,
+                                9000,
+                                "string",
+                                [],
+                                {}
+                            ],
+                            "object": {
+                                "boolean": true,
+                                "number": 9000,
+                                "string": "string",
+                                "array": [
+                                    null,
+                                    true,
+                                    9000,
+                                    "string",
+                                    [],
+                                    {}
+                                ],
+                                "object": null
+                            }
+                        }
+                    }
+                ]
+            }
+        """
+
+        guard let data = json.data(using: .utf8),
+              let manifest = Manifest.from(data: data),
+              let component = manifest.components.first,
+              let initialProperties = component.initialProperties else {
+            XCTFail("Failed to read manifest")
+            return
+        }
+
+        XCTAssertTrue(initialProperties["boolean"] as! Bool)
+        XCTAssertEqual(initialProperties["number"] as! Double, 9000)
+        XCTAssertEqual(initialProperties["string"] as! String, "string")
+
+        let array = initialProperties["array"] as! [Any]
+        XCTAssertNotNil(array[0] as? NSNull)
+        XCTAssertTrue(array[1] as! Bool)
+        XCTAssertEqual(array[2] as! Double, 9000)
+        XCTAssertEqual(array[3] as! String, "string")
+        XCTAssertNotNil(array[4] as? [Any])
+        XCTAssertNotNil(array[5] as? [AnyHashable: Any])
+
+        let object = initialProperties["object"] as! [AnyHashable: Any]
+        XCTAssertTrue(object["boolean"] as! Bool)
+        XCTAssertEqual(object["number"] as! Double, 9000)
+        XCTAssertEqual(object["string"] as! String, "string")
+
+        let innerArray = object["array"] as! [Any]
+        XCTAssertNotNil(innerArray[0] as? NSNull)
+        XCTAssertTrue(innerArray[1] as! Bool)
+        XCTAssertEqual(innerArray[2] as! Double, 9000)
+        XCTAssertEqual(innerArray[3] as! String, "string")
+        XCTAssertNotNil(innerArray[4] as? [Any])
+        XCTAssertNotNil(innerArray[5] as? [AnyHashable: Any])
+
+        XCTAssertNotNil(object["object"] as? NSNull)
+    }
 }
--- ios/ReactTestApp/Manifest.swift
+++ ios/ReactTestApp/Manifest.swift
@@ -7,13 +7,37 @@
 
 import Foundation
 
-struct Component: Codable {
+struct Component: Decodable {
     let appKey: String
     let displayName: String?
-    let initialProperties: [String: String]?
+    let initialProperties: [AnyHashable: Any]?
+
+    private enum Keys: String, CodingKey {
+        case appKey
+        case displayName
+        case initialProperties
+    }
+
+    init(appKey: String, displayName: String?, initialProperties: [AnyHashable: Any]?) {
+        self.appKey = appKey
+        self.displayName = displayName
+        self.initialProperties = initialProperties
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Keys.self)
+        appKey = try container.decode(String.self, forKey: .appKey)
+        displayName = try container.decodeIfPresent(String.self, forKey: .displayName)
+        initialProperties = {
+            guard let decoder = try? container.superDecoder(forKey: .initialProperties) else {
+                return nil
+            }
+            return try? [AnyHashable: Any].decode(from: decoder)
+        }()
+    }
 }
 
-struct Manifest: Codable {
+struct Manifest: Decodable {
     let name: String
     let displayName: String
     let components: [Component]
@@ -28,5 +52,81 @@ struct Manifest: Codable {
 
     static func from(data: Data) -> Manifest? {
         return try? JSONDecoder().decode(self, from: data)
+    }
+}
+
+private struct DynamicCodingKey: CodingKey {
+    private(set) var stringValue: String
+
+    init?(stringValue: String) {
+        self.stringValue = stringValue
+    }
+
+    private(set) var intValue: Int?
+
+    init?(intValue: Int) {
+        self.init(stringValue: "")
+        self.intValue = intValue
+    }
+
+    func toHashable() -> AnyHashable {
+        guard let intValue = intValue else {
+            return stringValue
+        }
+        return intValue
+    }
+}
+
+extension Array where Element == Any {
+    static func decode(from decoder: Decoder) throws -> Array? {
+        guard var container = try? decoder.unkeyedContainer() else {
+            return nil
+        }
+
+        var array: [Any] = []
+        while !container.isAtEnd {
+            if let isNull = try? container.decodeNil(), isNull {
+                array.append(NSNull())
+            } else if let boolean = try? container.decode(Bool.self) {
+                array.append(boolean)
+            } else if let double = try? container.decode(Double.self) {
+                array.append(double)
+            } else if let string = try? container.decode(String.self) {
+                array.append(string)
+            } else if let superDecoder = try? container.superDecoder() {
+                if let innerArray = try? Array.decode(from: superDecoder) {
+                    array.append(innerArray)
+                } else if let object = try? Dictionary.decode(from: superDecoder) {
+                    array.append(object)
+                }
+            }
+        }
+        return array
+    }
+}
+
+extension Dictionary where Key == AnyHashable, Value == Any {
+    static func decode(from decoder: Decoder) throws -> Dictionary? {
+        guard let container = try? decoder.container(keyedBy: DynamicCodingKey.self) else {
+            return nil
+        }
+
+        var dictionary: [AnyHashable: Any] = [:]
+        for key in container.allKeys {
+            if let isNull = try? container.decodeNil(forKey: key), isNull {
+                dictionary[key.toHashable()] = NSNull()
+            } else if let boolean = try? container.decode(Bool.self, forKey: key) {
+                dictionary[key.toHashable()] = boolean
+            } else if let double = try? container.decode(Double.self, forKey: key) {
+                dictionary[key.toHashable()] = double
+            } else if let string = try? container.decode(String.self, forKey: key) {
+                dictionary[key.toHashable()] = string
+            } else if let array = try? Array.decode(from: container.superDecoder(forKey: key)) {
+                dictionary[key.toHashable()] = array
+            } else if let object = try? Dictionary.decode(from: container.superDecoder(forKey: key)) {
+                dictionary[key.toHashable()] = object
+            }
+        }
+        return dictionary
     }
 }
