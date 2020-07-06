ec4243bd7f77e27a67ea51a41f9dd2a7ef17f556
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2020-07-06 13:33:40 +0200

    ci(macOS): Run tests on CI (#150)

---
+++ macos/ReactTestAppUITests/ReactTestAppUITests.swift
@@ -0,0 +1,12 @@
+//
+// Copyright (c) Microsoft Corporation
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+import XCTest
+
+class ReactTestAppUITests: XCTestCase {
+    // Stub to make tests compile
+}
