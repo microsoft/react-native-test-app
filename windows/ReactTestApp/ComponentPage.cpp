22324621d3723b094c4fbaf367ab364f6e0fd7fb
Anastasia Orishchenko <t-anoris@microsoft.com>
2020-07-10 09:24:52 +0200

    Loading components

--- windows/ReactTestApp/ComponentPage.cpp
+++ windows/ReactTestApp/ComponentPage.cpp
@@ -5,8 +5,13 @@
 #include "ComponentPage.g.cpp"
 #endif
 
+#include <App.h>
+
+#include "winrt/Windows.System.h"
+
 using namespace winrt;
 using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Core;
 
 namespace winrt::ReactTestApp::implementation
 {
@@ -15,4 +20,21 @@ namespace winrt::ReactTestApp::implementation
         InitializeComponent();
     }
 
+    void ComponentPage::OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs const &e)
+    {
+        auto app = Application::Current().as<App>();
+        ReactRootView().ReactNativeHost(app->Host());
+        ComponentViewModel component =
+            winrt::unbox_value<ReactTestApp::ComponentViewModel>(e.Parameter());
+        ReactRootView().ComponentName(component.AppKey());
+        __super::OnNavigatedTo(e);
+    }
+
+    void ComponentPage::OnBackButtonClicked(Windows::Foundation::IInspectable const &,
+                                            RoutedEventArgs)
+    {
+        if (Frame().CanGoBack()) {
+            Frame().GoBack();
+        }
+    }
 }  // namespace winrt::ReactTestApp::implementation
