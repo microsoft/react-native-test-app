2d66e21ae3077fb334bedd094d682d9ea4051964
Anastasia Orishchenko <t-anoris@microsoft.com>
2020-07-13 09:12:05 +0200

    Load from dev server by default

--- windows/ReactTestApp/App.h
+++ windows/ReactTestApp/App.h
@@ -12,13 +12,10 @@ namespace winrt::ReactTestApp::implementation
                           Windows::ApplicationModel::SuspendingEventArgs const &);
         void OnNavigationFailed(IInspectable const &,
                                 Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const &);
-        winrt::Microsoft::ReactNative::ReactNativeHost InitReact();
 
     private:
         void NavigateToFirstPage(
             Windows::UI::Xaml::Controls::Frame &rootFrame,
             Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const &e);
-        using base = AppT;
-        winrt::Microsoft::ReactNative::ReactApplicationDelegate m_AppDelegate;
     };
 }  // namespace winrt::ReactTestApp::implementation
