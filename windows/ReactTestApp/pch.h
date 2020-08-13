9517db37d31cb6e57ff46e2e750f9190c108f2eb
AnastasiaOrishchenko <42567951+AnastasiaOrishchenko@users.noreply.github.com>
2020-08-13 19:25:56 +0200

    fix(Windows): Add debug menu option and key accelerators to React component items (#194)

--- windows/ReactTestApp/pch.h
+++ windows/ReactTestApp/pch.h
@@ -10,9 +10,11 @@
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.UI.Input.Preview.Injection.h>
 #include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Windows.UI.Xaml.Controls.h>
 #include <winrt/Windows.UI.Xaml.Data.h>
+#include <winrt/Windows.UI.Xaml.Input.h>
 #include <winrt/Windows.UI.Xaml.Interop.h>
 #include <winrt/Windows.UI.Xaml.Markup.h>
 #include <winrt/Windows.UI.Xaml.Navigation.h>
