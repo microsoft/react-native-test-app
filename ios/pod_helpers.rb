9fa741d942b520f168e8bae2aac4f433700301e1
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2020-08-27 08:26:25 +0200

    fix(macOS): Enable Hermes if hermes-engine-darwin is installed (#198)

--- ios/pod_helpers.rb
+++ ios/pod_helpers.rb
@@ -5,6 +5,11 @@
 # LICENSE file in the root directory of this source tree.
 #
 
+def resolve_module(request)
+  script = "console.log(path.dirname(require.resolve('#{request}/package.json')));"
+  Pod::Executable.execute_command('node', ['-e', script], true).strip
+end
+
 def try_pod(name, podspec, project_root)
   pod name, :podspec => podspec if File.exist?(File.join(project_root, podspec))
 end
