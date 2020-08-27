b33b201c68f033103e1c51314caebf6d676d20fa
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2020-08-27 15:29:12 +0200

    ci: Fix clang-format diffs not being picked up when quotes are present (#200)

--- scripts/clang-format-diff.sh
+++ scripts/clang-format-diff.sh
@@ -5,4 +5,6 @@ if [[ -z $DEBUG ]]; then
   brew install clang-format
   curl --silent --show-error --remote-name https://raw.githubusercontent.com/llvm/llvm-project/release/10.x/clang/tools/clang-format/clang-format-diff.py
 fi
-yarn suggestion-bot "$(git diff --unified=0 --no-color @^ | python clang-format-diff.py -p1 -regex '.*\.(cpp|cc|c\+\+|cxx|c|cl|h|hh|hpp|m|mm|inc)' -sort-includes)"
+git diff --unified=0 --no-color @^ \
+  | python clang-format-diff.py -p1 -regex '.*\.(cpp|cc|c\+\+|cxx|c|cl|h|hh|hpp|m|mm|inc)' -sort-includes \
+  | yarn suggestion-bot
