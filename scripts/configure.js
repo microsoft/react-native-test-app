microsoft/react-native-test-app

commit 932c42d209facf394669bf7acd0062ed6741e70b

fix(ios): fix `run-ios` failing with `@react-native-community/cli` 5 (#445)

scripts/configure.js
@@ -150,6 +150,17 @@ function readJSONFile(path) {
 }
 
 /**
+ * Returns whether installed package satisfies specified version range.
+ * @param {string} pkg
+ * @param {string} versionRange
+ * @returns {boolean}
+ */
+function packageSatisfiesVersionRange(pkg, versionRange) {
+  const { version } = require(`${pkg}/package.json`);
+  return semver.satisfies(version, versionRange);
+}
+
+/**
  * Converts an object or value to a pretty JSON string.
  * @param {Record<string, unknown>} obj
  * @return {string}
@@ -239,10 +250,18 @@ function reactNativeConfig({ name, testAppPath, platforms, flatten }) {
       case "macos":
       case "ios":
         return join(
+          "const {",
+          "  packageSatisfiesVersionRange,",
+          '} = require("react-native-test-app/scripts/configure");',
           "module.exports = {",
           "  project: {",
           "    ios: {",
-          `      project: "ReactTestApp-Dummy.xcodeproj"`,
+          "      project: packageSatisfiesVersionRange(",
+          '        "@react-native-community/cli-platform-ios",',
+          '        "<5.0.2"',
+          "      )",
+          '        ? "ReactTestApp-Dummy.xcodeproj"',
+          '        : "node_modules/.generated/ios/ReactTestApp.xcodeproj",',
           "    }",
           "  }",
           "};",
@@ -889,22 +908,23 @@ if (require.main === module) {
   ).argv;
 }
 
-exports["configure"] = configure;
-exports["error"] = error;
-exports["gatherConfig"] = gatherConfig;
-exports["getAppName"] = getAppName;
-exports["getConfig"] = getConfig;
-exports["getPlatformPackage"] = getPlatformPackage;
-exports["getTargetReactNativeVersion"] = getTargetReactNativeVersion;
-exports["isDestructive"] = isDestructive;
-exports["isInstalled"] = isInstalled;
-exports["join"] = join;
-exports["mergeConfig"] = mergeConfig;
-exports["projectRelativePath"] = projectRelativePath;
-exports["reactNativeConfig"] = reactNativeConfig;
-exports["readJSONFile"] = readJSONFile;
-exports["removeAllFiles"] = removeAllFiles;
-exports["sortByKeys"] = sortByKeys;
-exports["updatePackageManifest"] = updatePackageManifest;
-exports["warn"] = warn;
-exports["writeAllFiles"] = writeAllFiles;
+exports.configure = configure;
+exports.error = error;
+exports.gatherConfig = gatherConfig;
+exports.getAppName = getAppName;
+exports.getConfig = getConfig;
+exports.getPlatformPackage = getPlatformPackage;
+exports.getTargetReactNativeVersion = getTargetReactNativeVersion;
+exports.isDestructive = isDestructive;
+exports.isInstalled = isInstalled;
+exports.join = join;
+exports.mergeConfig = mergeConfig;
+exports.packageSatisfiesVersionRange = packageSatisfiesVersionRange;
+exports.projectRelativePath = projectRelativePath;
+exports.reactNativeConfig = reactNativeConfig;
+exports.readJSONFile = readJSONFile;
+exports.removeAllFiles = removeAllFiles;
+exports.sortByKeys = sortByKeys;
+exports.updatePackageManifest = updatePackageManifest;
+exports.warn = warn;
+exports.writeAllFiles = writeAllFiles;

example/react-native.config.js
@@ -26,7 +26,26 @@ module.exports = {
       ),
     },
     ios: {
-      project: "ios/ReactTestApp-Dummy.xcodeproj",
+      project: (() => {
+        const {
+          packageSatisfiesVersionRange,
+        } = require("react-native-test-app/scripts/configure");
+        if (
+          packageSatisfiesVersionRange(
+            "@react-native-community/cli-platform-ios",
+            "<5.0.2"
+          )
+        ) {
+          // Prior to @react-native-community/cli-platform-ios v5.0.0,
+          // `project` was only used to infer `sourceDir` and `podfile`.
+          return "ios/ReactTestApp-Dummy.xcodeproj";
+        }
+
+        // `sourceDir` and `podfile` detection was fixed in
+        // @react-native-community/cli-platform-ios v5.0.2 (see
+        // https://github.com/react-native-community/cli/pull/1444).
+        return "node_modules/.generated/ios/ReactTestApp.xcodeproj";
+      })(),
     },
     windows: fs.existsSync(windowsProjectFile) && {
       sourceDir: "windows",

test/configure/__snapshots__/gatherConfig.test.js.snap
@@ -24,10 +24,18 @@ use_test_app!
     "metro.config.js": Object {
       "source": "example/metro.config.js",
     },
-    "react-native.config.js": "module.exports = {
+    "react-native.config.js": "const {
+  packageSatisfiesVersionRange,
+} = require(\\"react-native-test-app/scripts/configure\\");
+module.exports = {
   project: {
     ios: {
-      project: \\"ReactTestApp-Dummy.xcodeproj\\"
+      project: packageSatisfiesVersionRange(
+        \\"@react-native-community/cli-platform-ios\\",
+        \\"<5.0.2\\"
+      )
+        ? \\"ReactTestApp-Dummy.xcodeproj\\"
+        : \\"node_modules/.generated/ios/ReactTestApp.xcodeproj\\",
     }
   }
 };
@@ -142,7 +150,26 @@ module.exports = {
       ),
     },
     ios: {
-      project: \\"ios/ReactTestApp-Dummy.xcodeproj\\",
+      project: (() => {
+        const {
+          packageSatisfiesVersionRange,
+        } = require(\\"react-native-test-app/scripts/configure\\");
+        if (
+          packageSatisfiesVersionRange(
+            \\"@react-native-community/cli-platform-ios\\",
+            \\"<5.0.2\\"
+          )
+        ) {
+          // Prior to @react-native-community/cli-platform-ios v5.0.0,
+          // \`project\` was only used to infer \`sourceDir\` and \`podfile\`.
+          return \\"ios/ReactTestApp-Dummy.xcodeproj\\";
+        }
+
+        // \`sourceDir\` and \`podfile\` detection was fixed in
+        // @react-native-community/cli-platform-ios v5.0.2 (see
+        // https://github.com/react-native-community/cli/pull/1444).
+        return \\"node_modules/.generated/ios/ReactTestApp.xcodeproj\\";
+      })(),
     },
     windows: fs.existsSync(windowsProjectFile) && {
       sourceDir: \\"windows\\",
@@ -226,7 +253,26 @@ module.exports = {
       ),
     },
     ios: {
-      project: \\"ios/ReactTestApp-Dummy.xcodeproj\\",
+      project: (() => {
+        const {
+          packageSatisfiesVersionRange,
+        } = require(\\"react-native-test-app/scripts/configure\\");
+        if (
+          packageSatisfiesVersionRange(
+            \\"@react-native-community/cli-platform-ios\\",
+            \\"<5.0.2\\"
+          )
+        ) {
+          // Prior to @react-native-community/cli-platform-ios v5.0.0,
+          // \`project\` was only used to infer \`sourceDir\` and \`podfile\`.
+          return \\"ios/ReactTestApp-Dummy.xcodeproj\\";
+        }
+
+        // \`sourceDir\` and \`podfile\` detection was fixed in
+        // @react-native-community/cli-platform-ios v5.0.2 (see
+        // https://github.com/react-native-community/cli/pull/1444).
+        return \\"node_modules/.generated/ios/ReactTestApp.xcodeproj\\";
+      })(),
     },
     windows: fs.existsSync(windowsProjectFile) && {
       sourceDir: \\"windows\\",
@@ -360,7 +406,26 @@ module.exports = {
       ),
     },
     ios: {
-      project: \\"ios/ReactTestApp-Dummy.xcodeproj\\",
+      project: (() => {
+        const {
+          packageSatisfiesVersionRange,
+        } = require(\\"react-native-test-app/scripts/configure\\");
+        if (
+          packageSatisfiesVersionRange(
+            \\"@react-native-community/cli-platform-ios\\",
+            \\"<5.0.2\\"
+          )
+        ) {
+          // Prior to @react-native-community/cli-platform-ios v5.0.0,
+          // \`project\` was only used to infer \`sourceDir\` and \`podfile\`.
+          return \\"ios/ReactTestApp-Dummy.xcodeproj\\";
+        }
+
+        // \`sourceDir\` and \`podfile\` detection was fixed in
+        // @react-native-community/cli-platform-ios v5.0.2 (see
+        // https://github.com/react-native-community/cli/pull/1444).
+        return \\"node_modules/.generated/ios/ReactTestApp.xcodeproj\\";
+      })(),
     },
     windows: fs.existsSync(windowsProjectFile) && {
       sourceDir: \\"windows\\",
@@ -501,7 +566,26 @@ module.exports = {
       ),
     },
     ios: {
-      project: \\"ios/ReactTestApp-Dummy.xcodeproj\\",
+      project: (() => {
+        const {
+          packageSatisfiesVersionRange,
+        } = require(\\"react-native-test-app/scripts/configure\\");
+        if (
+          packageSatisfiesVersionRange(
+            \\"@react-native-community/cli-platform-ios\\",
+            \\"<5.0.2\\"
+          )
+        ) {
+          // Prior to @react-native-community/cli-platform-ios v5.0.0,
+          // \`project\` was only used to infer \`sourceDir\` and \`podfile\`.
+          return \\"ios/ReactTestApp-Dummy.xcodeproj\\";
+        }
+
+        // \`sourceDir\` and \`podfile\` detection was fixed in
+        // @react-native-community/cli-platform-ios v5.0.2 (see
+        // https://github.com/react-native-community/cli/pull/1444).
+        return \\"node_modules/.generated/ios/ReactTestApp.xcodeproj\\";
+      })(),
     },
     windows: fs.existsSync(windowsProjectFile) && {
       sourceDir: \\"windows\\",

commit d1da9136619c08f1e702da0f77d18bdbfb478064

chore: fix `pod install` breaking when symlinked
Regressed with #445.

scripts/configure.js
@@ -7,6 +7,8 @@
 //
 // @ts-check
 
+require("./link")(module);
+
 const chalk = require("chalk");
 const fs = require("fs");
 const path = require("path");

scripts/link.js
@@ -1,0 +1,15 @@
+// @ts-check
+
+const path = require("path");
+
+const localNodeModulesPath = path.join(process.cwd(), "node_modules");
+
+/** @type {(module: NodeJS.Module) => void} */
+module.exports = (module) => {
+  if (!module.paths.includes(localNodeModulesPath)) {
+    // Add the `node_modules` path whence the script was invoked. Without it,
+    // this script will fail to resolve any packages when
+    // `react-native-test-app` was linked using npm or yarn link.
+    module.paths.push(localNodeModulesPath);
+  }
+};

windows/test-app.js
@@ -547,13 +547,7 @@ function generateSolution(destPath, { autolink, useHermes, useNuGet }) {
 }
 
 if (require.main === module) {
-  const localNodeModulesPath = path.join(process.cwd(), "node_modules");
-  if (!module.paths.includes(localNodeModulesPath)) {
-    // Add the `node_modules` path whence the script was invoked. Without it,
-    // this script will fail to resolve any packages when
-    // `react-native-test-app` was linked using npm or yarn link.
-    module.paths.push(localNodeModulesPath);
-  }
+  require("../scripts/link")(module);
 
   require("yargs").usage(
     "$0 [options]",