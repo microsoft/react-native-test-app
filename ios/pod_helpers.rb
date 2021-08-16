be1f63d15628268f29cbd1acdcd67aff214e3cb6
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-08-16 10:29:56 +0200

    fix(apple): avoid invoking Node for module resolution (#450)

--- ios/pod_helpers.rb
+++ ios/pod_helpers.rb
@@ -5,13 +5,23 @@
 # LICENSE file in the root directory of this source tree.
 #
 
+def find_file(file_name, current_dir)
+  return if current_dir.expand_path.to_s == '/'
+
+  path = current_dir + file_name
+  return path if File.exist?(path)
+
+  find_file(file_name, current_dir.parent)
+end
+
 def resolve_module(request)
   @module_cache ||= {}
   return @module_cache[request] if @module_cache.key?(request)
 
-  script = "console.log(path.dirname(require.resolve('#{request}/package.json')));"
-  path = Pod::Executable.execute_command('node', ['-e', script], true).strip
-  @module_cache[request] = path
+  package_json = find_file("node_modules/#{request}/package.json", Pathname.pwd)
+  raise "Cannot find module '#{request}'" if package_json.nil?
+
+  @module_cache[request] = package_json.dirname.to_s
 end
 
 def try_pod(name, podspec, project_root)
