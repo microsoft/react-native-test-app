28025cf719cf04c3fbcd36dd21bbdbf19ce8a23e
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-10-22 08:51:18 +0200

    test(android): run unit tests locally to speed up Android CI

--- android/app/src/test/java/com/microsoft/reacttestapp/ManifestTest.kt
+++ android/app/src/test/java/com/microsoft/reacttestapp/ManifestTest.kt
@@ -1,5 +1,11 @@
 package com.microsoft.reacttestapp
 
+import android.os.Bundle
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.JavaOnlyArray
+import com.facebook.react.bridge.JavaOnlyMap
+import com.facebook.react.bridge.WritableArray
+import com.facebook.react.bridge.WritableMap
 import com.microsoft.reacttestapp.manifest.Manifest
 import com.microsoft.reacttestapp.manifest.ManifestJsonAdapter
 import com.microsoft.reacttestapp.manifest.MoshiBundleAdapter
@@ -9,7 +15,13 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.MockedConstruction
+import org.mockito.MockedStatic
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class ManifestTest {
     private lateinit var adapter: JsonAdapter<Manifest>
 
@@ -43,6 +55,9 @@ class ManifestTest {
             }
         """
 
+        val mockedBundle = mockBundle()
+        val mockedArguments = mockArguments()
+
         val manifest = adapter.fromJson(json)
         assertNotNull(manifest)
 
@@ -64,6 +79,39 @@ class ManifestTest {
             val propsTwo = componentTwo.initialProperties
             assertNotNull(propsTwo)
             assertEquals("value_2", propsTwo?.get("prop_2").toString())
+        }
+
+        mockedArguments.close()
+        mockedBundle.close()
+    }
+
+    private fun mockArguments(): MockedStatic<Arguments> {
+        val mockedArguments = Mockito.mockStatic(Arguments::class.java)
+        mockedArguments
+            .`when`<WritableArray> { Arguments.createArray() }
+            .thenReturn(JavaOnlyArray())
+        mockedArguments
+            .`when`<WritableMap> { Arguments.createMap() }
+            .thenReturn(JavaOnlyMap())
+        mockedArguments
+            .`when`<Bundle> { Arguments.toBundle(Mockito.any()) }
+            .thenAnswer { it.callRealMethod() }
+        return mockedArguments
+    }
+
+    private fun mockBundle(): MockedConstruction<Bundle> {
+        val map = HashMap<String, Any>()
+        return Mockito.mockConstruction(Bundle::class.java) { mock, _ ->
+            Mockito.doAnswer { map[it.getArgument(0)] }.`when`(mock).get(Mockito.anyString())
+
+            Mockito.doAnswer {
+                val key = it.getArgument<String>(0)
+                val value = it.getArgument<Any>(1)
+                map[key] = value
+                null
+            }.`when`(mock).putString(Mockito.anyString(), Mockito.any())
+
+            Mockito.doAnswer { map.toString() }.`when`(mock).toString()
         }
     }
 }
