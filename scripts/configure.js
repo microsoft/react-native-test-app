de48a9934afd5d4184b410b52ac37de16a0928d6
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-10-29 08:35:21 +0200

    fix: supported versions in init are out of sync

--- scripts/configure.js
+++ scripts/configure.js
@@ -197,15 +197,15 @@ function warn(message, tag = "[!]") {
  * Returns platform package at target version if it satisfies version range.
  * @param {string} packageName
  * @param {string} targetVersion
- * @param {string} versionRange
  * @returns {Record<string, string> | undefined}
  */
-function getPlatformPackage(packageName, targetVersion, versionRange) {
+function getPlatformPackage(packageName, targetVersion) {
   const v = semver.coerce(targetVersion);
   if (!v) {
     throw new Error(`Invalid ${packageName} version: ${targetVersion}`);
   }
 
+  const versionRange = require("../package.json").peerDependencies[packageName];
   if (!semver.satisfies(v.version, versionRange)) {
     warn(
       `${packageName}@${v.major}.${v.minor} cannot be added because it does not exist or is unsupported`
@@ -541,11 +541,7 @@ const getConfig = (() => {
           },
           dependencies: {},
           getDependencies: ({ targetVersion }) => {
-            return getPlatformPackage(
-              "react-native-macos",
-              targetVersion,
-              "^0.0.0-0 || >=0.60.0 <0.64"
-            );
+            return getPlatformPackage("react-native-macos", targetVersion);
           },
         },
         windows: {
@@ -571,11 +567,7 @@ const getConfig = (() => {
           },
           dependencies: {},
           getDependencies: ({ targetVersion }) => {
-            return getPlatformPackage(
-              "react-native-windows",
-              targetVersion,
-              "^0.0.0-0 || >=0.62.0 <0.66"
-            );
+            return getPlatformPackage("react-native-windows", targetVersion);
           },
         },
       };
