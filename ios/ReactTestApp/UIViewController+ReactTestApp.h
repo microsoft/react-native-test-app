aa7ca5396c96cc36a62d5309ec4cef5d1d364261
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-01-05 13:15:59 +0100

    docs: remove copyright header from all files (#667)

--- ios/ReactTestApp/UIViewController+ReactTestApp.h
+++ ios/ReactTestApp/UIViewController+ReactTestApp.h
@@ -1,10 +1,3 @@
-//
-// Copyright (c) Microsoft Corporation. All rights reserved.
-//
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
-//
-
 #include <TargetConditionals.h>
 #if TARGET_OS_IOS
 #import <UIKit/UIViewController.h>
--- test/windows-test-app/replaceContent.test.js
+++ test/windows-test-app/replaceContent.test.js
@@ -1,10 +1,5 @@
-//
-// Copyright (c) Microsoft Corporation
-//
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
-//
 // @ts-check
+"use strict";
 
 describe("replaceContent", () => {
   const { replaceContent } = require("../../windows/test-app");
