480e1170946d24e450764f425f1f13587b22612e
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-01-26 12:42:16 +0100

    fix(apple): prepare `react-native-codegen` when using main (#711)

--- ios/use_react_native-0.64.rb
+++ ios/use_react_native-0.64.rb
@@ -1,3 +1,5 @@
+require 'open3'
+
 def include_react_native!(options)
   react_native, flipper_versions, project_root, target_platform = options.values_at(
     :path, :rta_flipper_versions, :rta_project_root, :rta_target_platform
@@ -7,6 +9,11 @@ def include_react_native!(options)
 
   use_flipper!(flipper_versions) if target_platform == :ios && flipper_versions
   use_react_native!(options)
+
+  # If we're using react-native@main, we'll also need to prepare
+  # `react-native-codegen`.
+  codegen = File.join(project_root, react_native, 'packages', 'react-native-codegen')
+  Open3.popen3('yarn', :chdir => codegen) if File.directory?(codegen)
 
   lambda { |installer|
     react_native_post_install(installer)
