ad4f2c91e8aa1eb70dadce81fec66473aa426ee6
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-02-03 12:00:46 +0100

    Update scripts/patch-cli-platform-android.js
    Co-authored-by: Lorenzo Sciandra <lsciandra@microsoft.com>

--- scripts/patch-cli-platform-android.js
+++ scripts/patch-cli-platform-android.js
@@ -16,6 +16,8 @@ try {
   );
 
   const script = fs.readFileSync(nativeModulesScript, { encoding: "utf-8" });
+  // TODO: Remove when `@react-native-community/cli` 6.0+ is required. See also
+  // https://github.com/react-native-community/cli/commit/fa0d09b2c9be144bbdff526bb14f171d7ddca88e
   const patched = script.replace(
     "ArrayList<HashMap<String, String>>[] packages = this.reactNativeModules",
     "ArrayList<HashMap<String, String>> packages = this.reactNativeModules"
