ceadbc97341223cfd2a308b6837bb05a3ff6d55e
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-02-03 13:47:40 +0100

    fix(android): workaround for `Java heap space` when patching CLI (#740)

---
+++ scripts/patch-cli-platform-android.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+// @ts-check
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+try {
+  const nativeModulesScript = path.join(
+    path.dirname(
+      require.resolve(
+        "@react-native-community/cli-platform-android/package.json"
+      )
+    ),
+    "native_modules.gradle"
+  );
+
+  const script = fs.readFileSync(nativeModulesScript, { encoding: "utf-8" });
+  // TODO: Remove when `@react-native-community/cli` 6.0+ is required. See also
+  // https://github.com/react-native-community/cli/commit/fa0d09b2c9be144bbdff526bb14f171d7ddca88e
+  const patched = script.replace(
+    "ArrayList<HashMap<String, String>>[] packages = this.reactNativeModules",
+    "ArrayList<HashMap<String, String>> packages = this.reactNativeModules"
+  );
+  if (patched !== script) {
+    fs.writeFileSync(nativeModulesScript, patched);
+  }
+} catch (_) {
+  // Ignore if we cannot patch
+}
