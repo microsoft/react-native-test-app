4911f3b0c213c69f81fe144246269deaaf44b164
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-03-30 15:23:00 +0200

    fix: cannot find `react-native-test-app` when Metro starts (#839)
    `react-native.config.js` is imported by `@react-native-community/cli`
    and may output warnings about not being able to find
    `react-native-test-app` if the library it's looking at uses our utility
    functions.

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,24 +1,31 @@
-const fs = require("fs");
-const path = require("path");
-const {
-  androidManifestPath,
-  iosProjectPath,
-  windowsProjectPath,
-} = require("react-native-test-app");
+const project = (() => {
+  const fs = require("fs");
+  const path = require("path");
+  try {
+    const {
+      androidManifestPath,
+      iosProjectPath,
+      windowsProjectPath,
+    } = require("react-native-test-app");
+    return {
+      android: {
+        sourceDir: "android",
+        manifestPath: androidManifestPath(path.join(__dirname, "android")),
+      },
+      ios: {
+        project: iosProjectPath("ios"),
+      },
+      windows: fs.existsSync("windows/Example.sln") && {
+        sourceDir: "windows",
+        solutionFile: "Example.sln",
+        project: windowsProjectPath(path.join(__dirname, "windows")),
+      },
+    };
+  } catch (_) {
+    return undefined;
+  }
+})();
 
 module.exports = {
-  project: {
-    android: {
-      sourceDir: "android",
-      manifestPath: androidManifestPath(path.join(__dirname, "android")),
-    },
-    ios: {
-      project: iosProjectPath("ios"),
-    },
-    windows: fs.existsSync("windows/Example.sln") && {
-      sourceDir: "windows",
-      solutionFile: "Example.sln",
-      project: windowsProjectPath(path.join(__dirname, "windows")),
-    },
-  },
+  ...(project ? { project } : undefined),
 };
