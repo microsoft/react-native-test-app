8f5c619187c0fe021a24dd9176edee0a1789ed73
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-06-07 14:42:20 +0200

    chore: don't publish internal scripts

--- scripts/shasum.mjs
+++ scripts/shasum.mjs
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 // @ts-check
 
+import { createHash } from "node:crypto";
+import { readFileSync } from "node:fs";
+
 /**
  * @param {string} filename
  * @param {string=} algorithm
  * @returns {string}
  */
 function hashFile(filename, algorithm = "sha256") {
-  const data = require("fs")
-    .readFileSync(filename, { encoding: "utf-8" })
-    .replace(/\r/g, "");
-  return require("crypto").createHash(algorithm).update(data).digest("hex");
+  const data = readFileSync(filename, { encoding: "utf-8" }).replace(/\r/g, "");
+  return createHash(algorithm).update(data).digest("hex");
 }
 
 const { [2]: filename } = process.argv;
