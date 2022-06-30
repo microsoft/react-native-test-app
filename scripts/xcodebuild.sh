65ecd919a647d83819b65ee4ea1b180c6c0115f9
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-06-30 11:09:19 +0200

    Tests must be run on a concrete device

--- scripts/xcodebuild.sh
+++ scripts/xcodebuild.sh
@@ -8,7 +8,17 @@ shift 2
 platform=$(grep -o '\w\+/ReactTestApp.xcodeproj' "$workspace/contents.xcworkspacedata")
 
 if [[ $platform == ios/* ]]; then
-  destination='-destination "generic/platform=iOS Simulator"'
+  if [[ $action == 'test' || $action == 'test-without-building' ]]; then
+    device_name=${1:-'iPhone 13'}
+    device=$(xcrun simctl list devices "${device_name}" available | grep "${device_name} (")
+    re='\(([-0-9A-Fa-f]+)\)'
+    [[ $device =~ $re ]] || exit 1
+    shift || true
+    destination="-destination \"platform=iOS Simulator,id=${BASH_REMATCH[1]}\""
+  else
+    destination='-destination "generic/platform=iOS Simulator"'
+  fi
+
   skip_testing='-skip-testing:ReactTestAppTests/ReactNativePerformanceTests'
 elif [[ $platform == macos/* ]]; then
   destination=''
