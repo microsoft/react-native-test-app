25e8bc140936b98cfe1e3d46a77968a860e67906
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-09-01 09:16:00 +0200

    chore: use ccache's symlinks instead of own script

--- scripts/clang-format-diff.sh
+++ scripts/clang-format-diff.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 if [[ -z $DEBUG ]]; then
   brew install clang-format
-  curl --silent --show-error --remote-name https://raw.githubusercontent.com/llvm/llvm-project/release/10.x/clang/tools/clang-format/clang-format-diff.py
+  curl --silent --show-error --remote-name https://raw.githubusercontent.com/llvm/llvm-project/release/14.x/clang/tools/clang-format/clang-format-diff.py
 fi
 git diff --unified=0 --no-color @^ \
   | python clang-format-diff.py -p1 -regex '.*\.(cpp|cc|c\+\+|cxx|c|cl|h|hh|hpp|m|mm|inc)' -sort-includes \
