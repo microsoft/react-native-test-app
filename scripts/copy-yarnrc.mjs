ad9ed89235a4a7f80563e2012d6375b0d93f6b05
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-01-17 12:37:48 +0100

    ci: copy `.yarnrc.yaml` proper

---
+++ scripts/copy-yarnrc.mjs
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+// @ts-check
+
+import yaml from "js-yaml";
+import fs from "node:fs";
+import path from "node:path";
+
+/**
+ * Copies specied `.yarnrc.yaml`.
+ * @param {string} src
+ * @param {string} dst
+ */
+function main(src, dst) {
+  [src, dst].forEach((str) => {
+    if (!str || typeof str !== "string") {
+      throw new Error(`Invalid argument: ${str}`);
+    }
+  });
+
+  const yml = fs.readFileSync(src, { encoding: "utf-8" });
+  const rc = /** @type {Record<string, string | undefined>} */ (yaml.load(yml));
+
+  rc["nmHoistingLimits"] = undefined;
+  if (rc.yarnPath) {
+    rc["yarnPath"] = path.join(path.dirname(src), rc.yarnPath);
+  }
+
+  fs.writeFileSync(dst, yaml.dump(rc));
+}
+
+const { [2]: source, [3]: destination = ".yarnrc.yml" } = process.argv;
+main(source, destination);
