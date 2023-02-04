247c81260348dc2a06f7501a0c1a4cb1cb7122ad
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-02-04 12:21:09 +0100

    chore: bump Yarn to 3.4.1 (#1286)

---
+++ .yarn/plugins/link-project.js
@@ -0,0 +1,39 @@
+module.exports = {
+  name: "plugin-link-project",
+  factory: (_require) => ({
+    hooks: {
+      afterAllInstalled(project, _options) {
+        // As of 3.3.0, Yarn avoids creating circular symlinks. We need to
+        // manually create a symlink to the main project after install.
+        if (project.configuration.values.get("nodeLinker") !== "node-modules") {
+          return;
+        }
+
+        const fs = require("node:fs");
+        const path = require("node:path");
+
+        function normalize(p) {
+          // On Windows, paths are prefixed with `/`
+          return p.replace(/^[/\\]([^/\\]+:[/\\])/, "$1");
+        }
+
+        const projectPath = normalize(project.cwd);
+        const manifestPath = path.join(projectPath, "package.json");
+        const manifest = fs.readFileSync(manifestPath, { encoding: "utf-8" });
+        const { name } = JSON.parse(manifest);
+
+        for (const ws of project.workspaces) {
+          if (ws.cwd === project.cwd) {
+            continue;
+          }
+
+          const nodeModulesPath = path.join(normalize(ws.cwd), "node_modules");
+          const linkPath = path.join(nodeModulesPath, name);
+
+          fs.rmSync(linkPath, { force: true, maxRetries: 3, recursive: true });
+          fs.symlinkSync(path.relative(nodeModulesPath, projectPath), linkPath);
+        }
+      },
+    },
+  }),
+};
