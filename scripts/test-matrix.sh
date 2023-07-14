e0a88230db6ed00a80b042a9a7f843ac55953ace
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-07-14 13:08:43 +0200

    hardcode 'iPhone 14' for now

--- scripts/test-matrix.sh
+++ scripts/test-matrix.sh
@@ -9,7 +9,11 @@ VERSION=${1}
 scripts_dir=$(cd -P "$(dirname "$0")" && pwd)
 
 function build_and_run {
-  ${PACKAGE_MANAGER} $1 --no-packager
+  if [[ $1 == "ios" ]]; then
+    ${PACKAGE_MANAGER} $1 --simulator 'iPhone 14' --no-packager
+  else
+    ${PACKAGE_MANAGER} $1 --no-packager
+  fi
 }
 
 function pod_install {
