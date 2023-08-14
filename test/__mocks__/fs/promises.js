20056d6ea82c3df38720936e931becb86e41aa06
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-08-14 14:19:02 +0200

    refactor: simplify `writeAllFiles()`

--- test/__mocks__/fs/promises.js
+++ test/__mocks__/fs/promises.js
@@ -4,6 +4,9 @@ const fs = jest.createMockFromModule("fs/promises");
 
 const { vol } = require("memfs");
 
+fs.copyFile = (...args) => vol.promises.copyFile(...args);
+fs.mkdir = (...args) => vol.promises.mkdir(...args);
 fs.rm = (...args) => vol.promises.rm(...args);
+fs.writeFile = (...args) => vol.promises.writeFile(...args);
 
 module.exports = fs;
