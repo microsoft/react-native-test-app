518216931309f000fd52d9fbf6bd0f3d3459f3e7
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-09-13 12:34:44 +0200

    chore: extract config files

---
+++ .github/eslint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "plugin:@microsoft/sdl/required",
+    "plugin:@rnx-kit/recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+  ],
+  ignorePatterns: ["!.yarn"],
+};
