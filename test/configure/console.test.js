0e2da28e589ec1ab848ae564182feecc988156b5
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-09-25 22:41:57 +0200

    chore: remove unused `isInstalled`

--- test/configure/console.test.js
+++ test/configure/console.test.js
@@ -28,21 +28,3 @@ describe("console", () => {
     expect(consoleWarnSpy).toHaveBeenCalledTimes(1);
   });
 });
-
-describe("isInstalled()", () => {
-  const { isInstalled } = require("../../scripts/configure");
-
-  test("finds installed packages", () => {
-    expect(isInstalled("react-native", false)).toBe(true);
-    expect(isInstalled("this-package-does-not-exist-probably", false)).toBe(
-      false
-    );
-  });
-
-  test("throws if a required package is not found", () => {
-    expect(isInstalled("react-native", true)).toBe(true);
-    expect(() =>
-      isInstalled("this-package-does-not-exist-probably", true)
-    ).toThrow();
-  });
-});
