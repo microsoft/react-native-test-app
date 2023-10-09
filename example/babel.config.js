a4c3b6877260cc3ed6206ebf78cf5713a7958ce8
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-10-09 10:29:33 +0200

    fixup! fix(android): bump target API level to 33

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: (() => {
+    try {
+      return [require.resolve("@react-native/babel-preset")];
+    } catch (_) {
+      return ["module:metro-react-native-babel-preset"];
+    }
+  })(),
 };
