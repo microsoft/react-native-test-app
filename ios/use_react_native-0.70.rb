b4e7767d79b18494cd1772e36405988a77014c97
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-10-24 09:06:45 +0200

    fix: add Flipper deprecation warning (#1655)

--- ios/use_react_native-0.70.rb
+++ ios/use_react_native-0.70.rb
@@ -12,26 +12,38 @@ def include_react_native!(options)
 
   require_relative(File.join(project_root, react_native, 'scripts', 'react_native_pods'))
 
-  if target_platform == :ios && flipper_versions
-    Pod::UI.warn(
-      'use_flipper is deprecated from 0.70; use the flipper_configuration ' \
-      'option in the use_test_app! function instead if you don\'t need to ' \
-      'support older versions of react-native.'
-    )
-    options[:flipper_configuration] = FlipperConfiguration.enabled(['Debug'], flipper_versions)
+  if target_platform == :ios && (flipper_versions || options.key?(:flipper_configuration))
+    Pod::UI.warn('Flipper is deprecated and is removed from react-native in 0.74')
+    Pod::UI.warn('Flipper will be removed from react-native-test-app in 3.0')
+    if flipper_versions && defined?(FlipperConfiguration)
+      options[:flipper_configuration] = FlipperConfiguration.enabled(['Debug'], flipper_versions)
+    end
   end
 
   use_new_architecture!(options)
-  use_react_native!(
-    path: react_native,
-    fabric_enabled: options[:fabric_enabled] == true,
-    new_arch_enabled: options[:new_arch_enabled] == true,
-    production: options.key?(:production) ? options[:production] : ENV['PRODUCTION'] == '1',
-    hermes_enabled: options[:hermes_enabled] == true,
-    flipper_configuration: options[:flipper_configuration] || FlipperConfiguration.disabled,
-    app_path: options[:app_path] || '..',
-    config_file_dir: options[:config_file_dir] || ''
-  )
+  if defined?(FlipperConfiguration)
+    # TODO: Remove this branch in 3.0
+    use_react_native!(
+      path: react_native,
+      fabric_enabled: options[:fabric_enabled] == true,
+      new_arch_enabled: options[:new_arch_enabled] == true,
+      production: options.key?(:production) ? options[:production] : ENV['PRODUCTION'] == '1',
+      hermes_enabled: options[:hermes_enabled] == true,
+      flipper_configuration: options[:flipper_configuration] || FlipperConfiguration.disabled,
+      app_path: options[:app_path] || '..',
+      config_file_dir: options[:config_file_dir] || ''
+    )
+  else
+    use_react_native!(
+      path: react_native,
+      fabric_enabled: options[:fabric_enabled] == true,
+      new_arch_enabled: options[:new_arch_enabled] == true,
+      production: options.key?(:production) ? options[:production] : ENV['PRODUCTION'] == '1',
+      hermes_enabled: options[:hermes_enabled] == true,
+      app_path: options[:app_path] || '..',
+      config_file_dir: options[:config_file_dir] || ''
+    )
+  end
 
   # If we're using react-native@main, we'll also need to prepare
   # `react-native-codegen`.
