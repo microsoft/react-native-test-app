a3cf09186453355a18abf1d94c39f40ca022c782
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-01-16 14:09:59 +0100

    fix(apple): update `sourceURL(for:)` signature (#1771)
    Nullability annotations were added to the interface:
    https://github.com/facebook/react-native/commit/a7c5c2821c39a7bf653919353897d27d29a64068

--- ios/ReactTestApp/ReactInstance.swift
+++ ios/ReactTestApp/ReactInstance.swift
@@ -5,8 +5,16 @@ final class ReactInstance: NSObject, RNXHostConfig {
     public static let scanForQRCodeNotification =
         NSNotification.Name("ReactInstance.scanForQRCodeNotification")
 
-    static func jsBundleURL() -> URL? {
-        RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index") { nil }
+    static func jsBundleURL() -> URL {
+        RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index") {
+            RCTBundleURLProvider.jsBundleURL(
+                forBundleRoot: "index",
+                packagerHost: "localhost",
+                enableDev: true,
+                enableMinification: false,
+                inlineSourceMap: true
+            )
+        }
     }
 
     var remoteBundleURL: URL? {
@@ -105,20 +113,20 @@ final class ReactInstance: NSObject, RNXHostConfig {
 
     // MARK: - RCTBridgeDelegate details
 
-    func sourceURL(for _: RCTBridge!) -> URL? {
+    func sourceURL(for _: RCTBridge) -> URL {
         if let remoteBundleURL {
             return remoteBundleURL
         }
 
         let embeddedBundleURL = entryFiles()
             .lazy
-            .map {
+            .compactMap {
                 Bundle.main.url(
                     forResource: $0,
                     withExtension: "jsbundle"
                 )
             }
-            .first(where: { $0 != nil })
+            .first
         return embeddedBundleURL ?? ReactInstance.jsBundleURL()
     }
 
