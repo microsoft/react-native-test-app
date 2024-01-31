b4c7e17cc02cd1813a48d8ae8a230958dab92684
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-01-31 17:22:35 +0100

    fix(android): fix TurboModule modules not loading properly (#1805)

--- android/app/src/main/jni/OnLoad.cpp
+++ android/app/src/main/jni/OnLoad.cpp
@@ -1,3 +1,40 @@
+#if __has_include(<DefaultTurboModuleManagerDelegate.h>)
+
+#include <DefaultComponentsRegistry.h>
+#include <DefaultTurboModuleManagerDelegate.h>
+#include <rncli.h>
+
+#include <fbjni/fbjni.h>
+
+#include <ReactCommon/CallInvoker.h>
+
+using facebook::react::CallInvoker;
+using facebook::react::DefaultComponentsRegistry;
+using facebook::react::DefaultTurboModuleManagerDelegate;
+using facebook::react::TurboModule;
+
+namespace
+{
+    std::shared_ptr<TurboModule> cxxModuleProvider(const std::string &,
+                                                   const std::shared_ptr<CallInvoker> &)
+    {
+        return nullptr;
+    }
+}  // namespace
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *)
+{
+    return facebook::jni::initialize(vm, [] {
+        DefaultTurboModuleManagerDelegate::cxxModuleProvider = &cxxModuleProvider;
+        DefaultTurboModuleManagerDelegate::javaModuleProvider =
+            &facebook::react::rncli_ModuleProvider;
+        DefaultComponentsRegistry::registerComponentDescriptorsFromEntryPoint =
+            &facebook::react::rncli_registerProviders;
+    });
+}
+
+#else  // __has_include(<DefaultTurboModuleManagerDelegate.h>)
+
 #include <fbjni/fbjni.h>
 
 #include "ComponentsRegistry.h"
@@ -13,3 +50,5 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *)
         ComponentsRegistry::registerNatives();
     });
 }
+
+#endif  // __has_include(<DefaultTurboModuleManagerDelegate.h>)
