f78eff4aeb38e0f5a75126fd856775a323e04172
Kræn Hansen <kraen.hansen@mongodb.com>
2024-06-10 15:29:31 +0200

    Adding two more assertions

--- test/test_pod_helpers.rb
+++ test/test_pod_helpers.rb
@@ -76,6 +76,8 @@ class TestPodHelpers < Minitest::Test
 
     refute(new_architecture_enabled?({}, v(0, 70, 999)))
     refute(new_architecture_enabled?({}, available_version))
+    refute(new_architecture_enabled?({ fabric_enabled: true }, available_version))
+    refute(new_architecture_enabled?({ turbomodule_enabled: true }, available_version))
 
     ENV.delete('RCT_NEW_ARCH_ENABLED')
   end
