fffc1d7a54b4c9b966283aed0a19672ebe23163d
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-09-18 19:07:35 +0200

    fix(android): initialize SoLoader with merged libraries on 0.76 (#2223)

---
+++ android/app/src/reactapplication-0.76/java/com/microsoft/reacttestapp/TestApp.kt
@@ -0,0 +1,69 @@
+package com.microsoft.reacttestapp
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
+import com.facebook.soloader.SoLoader
+import com.microsoft.reacttestapp.manifest.Manifest
+import com.microsoft.reacttestapp.manifest.ManifestProvider
+import com.microsoft.reacttestapp.react.ReactBundleNameProvider
+import com.microsoft.reacttestapp.react.TestAppReactNativeHost
+import com.microsoft.reacttestapp.support.ReactTestAppLifecycleEvents
+
+class TestApp :
+    Application(),
+    ReactApplication {
+
+    val bundleNameProvider: ReactBundleNameProvider
+        get() = reactNativeBundleNameProvider
+
+    val manifest: Manifest by lazy {
+        ManifestProvider.manifest()
+    }
+
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+
+    override val reactNativeHost: TestAppReactNativeHost
+        get() = reactNativeHostInternal
+
+    private lateinit var reactNativeBundleNameProvider: ReactBundleNameProvider
+    private lateinit var reactNativeHostInternal: TestAppReactNativeHost
+
+    fun reloadJSFromServer(activity: Activity, bundleURL: String) {
+        reactNativeHostInternal.reloadJSFromServer(activity, bundleURL)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        SoLoader.init(this, OpenSourceMergedSoMapping)
+
+        reactNativeBundleNameProvider = ReactBundleNameProvider(this, manifest.bundleRoot)
+        reactNativeHostInternal =
+            TestAppReactNativeHost(this, reactNativeBundleNameProvider)
+
+        val eventConsumers = PackageList(this).packages
+            .filter { it is ReactTestAppLifecycleEvents }
+            .map { it as ReactTestAppLifecycleEvents }
+
+        eventConsumers.forEach { it.onTestAppInitialized() }
+
+        reactNativeHostInternal.init(
+            beforeReactNativeInit = {
+                eventConsumers.forEach { it.onTestAppWillInitializeReactNative() }
+            },
+            afterReactNativeInit = {
+                eventConsumers.forEach { it.onTestAppDidInitializeReactNative() }
+            }
+        )
+    }
+}
+
+val Context.testApp: TestApp
+    get() = applicationContext as TestApp
