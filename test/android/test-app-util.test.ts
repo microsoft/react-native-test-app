afa75aa507d88cd3bf93d6476bb649d4722c3fbb
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-10-22 20:05:23 +0200

    fix(android): avoid loading legacy autolinking script (#2291)

--- test/android/test-app-util.test.ts
+++ test/android/test-app-util.test.ts
@@ -8,10 +8,6 @@ import {
   runGradleWithProject,
 } from "./gradle.js";
 
-// TODO: https://github.com/microsoft/react-native-test-app/issues/2206
-const GRADLE_HANGS_ON_WINDOWS = process.platform === "win32";
-const testOptions = { skip: GRADLE_HANGS_ON_WINDOWS };
-
 describe("test-app-util.gradle", () => {
   const defaultTestProject = "TestAppUtilTest";
 
@@ -25,12 +21,10 @@ describe("test-app-util.gradle", () => {
   }
 
   after(() => {
-    if (!testOptions.skip) {
-      removeProject(defaultTestProject);
-    }
+    removeProject(defaultTestProject);
   });
 
-  it("getAppName() returns `displayName`", testOptions, async () => {
+  it("getAppName() returns `displayName`", async () => {
     const { status, stdout } = await runGradle({
       "app.json": JSON.stringify({
         name: "AppName",
@@ -46,7 +40,7 @@ describe("test-app-util.gradle", () => {
     match(stdout, /getAppName\(\) = AppDisplayName/);
   });
 
-  it("getApplicationId() returns default id", testOptions, async () => {
+  it("getApplicationId() returns default id", async () => {
     const { status, stdout } = await runGradle({
       "app.json": JSON.stringify({
         name: "AppName",
@@ -62,7 +56,7 @@ describe("test-app-util.gradle", () => {
     match(stdout, /getApplicationId\(\) = com.microsoft.reacttestapp/);
   });
 
-  it("getApplicationId() returns package name", testOptions, async () => {
+  it("getApplicationId() returns package name", async () => {
     const { status, stdout } = await runGradle({
       "app.json": JSON.stringify({
         name: "AppName",
@@ -81,140 +75,116 @@ describe("test-app-util.gradle", () => {
     match(stdout, /getApplicationId\(\) = com.contoso.application.id/);
   });
 
-  it(
-    "getPackageVersionNumber() returns `react-native` version as a number",
-    testOptions,
-    async () => {
-      const { status, stdout } = await runGradle({
-        "android/build.gradle": buildGradle(
-          'println("getPackageVersionNumber() = " + project.ext.getPackageVersionNumber("react-native", rootDir))'
-        ),
-      });
+  it("getPackageVersionNumber() returns `react-native` version as a number", async () => {
+    const { status, stdout } = await runGradle({
+      "android/build.gradle": buildGradle(
+        'println("getPackageVersionNumber() = " + project.ext.getPackageVersionNumber("react-native", rootDir))'
+      ),
+    });
 
-      const versionNumber = toVersionNumber(reactNativeVersion());
+    const versionNumber = toVersionNumber(reactNativeVersion());
 
-      equal(status, 0);
-      match(
-        stdout,
-        new RegExp(`getPackageVersionNumber\\(\\) = ${versionNumber}`)
-      );
-    }
-  );
+    equal(status, 0);
+    match(
+      stdout,
+      new RegExp(`getPackageVersionNumber\\(\\) = ${versionNumber}`)
+    );
+  });
 
-  it(
-    "getSigningConfigs() fails if `storeFile` is missing",
-    testOptions,
-    async () => {
-      const { status, stderr } = await runGradle({
-        "app.json": JSON.stringify({
-          name: "AppName",
-          displayName: "AppDisplayName",
-          resources: ["dist/res", "dist/main.android.jsbundle"],
-          android: { signingConfigs: { debug: {} } },
-        }),
-        "android/build.gradle": buildGradle(
-          'println("getSigningConfigs() = " + project.ext.getSigningConfigs())'
-        ),
-      });
+  it("getSigningConfigs() fails if `storeFile` is missing", async () => {
+    const { status, stderr } = await runGradle({
+      "app.json": JSON.stringify({
+        name: "AppName",
+        displayName: "AppDisplayName",
+        resources: ["dist/res", "dist/main.android.jsbundle"],
+        android: { signingConfigs: { debug: {} } },
+      }),
+      "android/build.gradle": buildGradle(
+        'println("getSigningConfigs() = " + project.ext.getSigningConfigs())'
+      ),
+    });
 
-      equal(status, 1);
-      match(stderr, /storeFile .* is missing/);
-    }
-  );
+    equal(status, 1);
+    match(stderr, /storeFile .* is missing/);
+  });
 
-  it(
-    "getSigningConfigs() skips empty `signingConfigs` config",
-    testOptions,
-    async () => {
-      const { status, stdout } = await runGradle({
-        "app.json": JSON.stringify({
-          name: "AppName",
-          displayName: "AppDisplayName",
-          resources: ["dist/res", "dist/main.android.jsbundle"],
-          android: { signingConfigs: {} },
-        }),
-        "android/build.gradle": buildGradle(
-          'println("getSigningConfigs() = " + project.ext.getSigningConfigs())'
-        ),
-      });
+  it("getSigningConfigs() skips empty `signingConfigs` config", async () => {
+    const { status, stdout } = await runGradle({
+      "app.json": JSON.stringify({
+        name: "AppName",
+        displayName: "AppDisplayName",
+        resources: ["dist/res", "dist/main.android.jsbundle"],
+        android: { signingConfigs: {} },
+      }),
+      "android/build.gradle": buildGradle(
+        'println("getSigningConfigs() = " + project.ext.getSigningConfigs())'
+      ),
+    });
 
-      equal(status, 0);
-      match(stdout, /getSigningConfigs\(\) = \[:\]/);
-    }
-  );
+    equal(status, 0);
+    match(stdout, /getSigningConfigs\(\) = \[:\]/);
+  });
 
-  it(
-    "getSigningConfigs() returns debug signing config",
-    testOptions,
-    async () => {
-      const { status, stdout } = await runGradle({
-        "app.json": JSON.stringify({
-          name: "AppName",
-          displayName: "AppDisplayName",
-          resources: ["dist/res", "dist/main.android.jsbundle"],
-          android: {
-            signingConfigs: {
-              debug: {
-                storeFile: "../README.md",
-              },
+  it("getSigningConfigs() returns debug signing config", async () => {
+    const { status, stdout } = await runGradle({
+      "app.json": JSON.stringify({
+        name: "AppName",
+        displayName: "AppDisplayName",
+        resources: ["dist/res", "dist/main.android.jsbundle"],
+        android: {
+          signingConfigs: {
+            debug: {
+              storeFile: "../README.md",
             },
           },
-        }),
-        "android/build.gradle": buildGradle(
-          'println("getSigningConfigs() = " + project.ext.getSigningConfigs())'
-        ),
-      });
+        },
+      }),
+      "android/build.gradle": buildGradle(
+        'println("getSigningConfigs() = " + project.ext.getSigningConfigs())'
+      ),
+    });
 
-      equal(status, 0);
-      match(
-        stdout,
-        /getSigningConfigs\(\) = \[debug:\[keyAlias:androiddebugkey, keyPassword:android, storePassword:android, storeFile:.*\]\]/
-      );
-    }
-  );
+    equal(status, 0);
+    match(
+      stdout,
+      /getSigningConfigs\(\) = \[debug:\[keyAlias:androiddebugkey, keyPassword:android, storePassword:android, storeFile:.*\]\]/
+    );
+  });
 
-  it(
-    "getSigningConfigs() returns release signing config",
-    testOptions,
-    async () => {
-      const { status, stdout } = await runGradle({
-        "app.json": JSON.stringify({
-          name: "AppName",
-          displayName: "AppDisplayName",
-          resources: ["dist/res", "dist/main.android.jsbundle"],
-          android: {
-            signingConfigs: {
-              release: {
-                storeFile: "../README.md",
-              },
+  it("getSigningConfigs() returns release signing config", async () => {
+    const { status, stdout } = await runGradle({
+      "app.json": JSON.stringify({
+        name: "AppName",
+        displayName: "AppDisplayName",
+        resources: ["dist/res", "dist/main.android.jsbundle"],
+        android: {
+          signingConfigs: {
+            release: {
+              storeFile: "../README.md",
             },
           },
-        }),
-        "android/build.gradle": buildGradle(
-          'println("getSigningConfigs() = " + project.ext.getSigningConfigs())'
-        ),
-      });
+        },
+      }),
+      "android/build.gradle": buildGradle(
+        'println("getSigningConfigs() = " + project.ext.getSigningConfigs())'
+      ),
+    });
 
-      equal(status, 0);
-      match(
-        stdout,
-        /getSigningConfigs\(\) = \[release:\[keyAlias:androiddebugkey, keyPassword:android, storePassword:android, storeFile:.*\]\]/
-      );
-    }
-  );
+    equal(status, 0);
+    match(
+      stdout,
+      /getSigningConfigs\(\) = \[release:\[keyAlias:androiddebugkey, keyPassword:android, storePassword:android, storeFile:.*\]\]/
+    );
+  });
 
-  it(
-    "toVersionNumber() handles pre-release identifiers",
-    testOptions,
-    async () => {
-      const { status, stdout } = await runGradle({
-        "android/build.gradle": buildGradle(
-          'println("toVersionNumber() = " + project.ext.toVersionNumber("1.2.3-053c2b4be"))'
-        ),
-      });
+  it("toVersionNumber() handles pre-release identifiers", async () => {
+    const { status, stdout } = await runGradle({
+      "android/build.gradle": buildGradle(
+        'println("toVersionNumber() = " + project.ext.toVersionNumber("1.2.3-053c2b4be"))'
+      ),
+    });
 
-      equal(status, 0);
-      match(stdout, new RegExp(`toVersionNumber\\(\\) = ${v(1, 2, 3)}`));
-    }
-  );
+    equal(status, 0);
+    match(stdout, new RegExp(`toVersionNumber\\(\\) = ${v(1, 2, 3)}`));
+  });
 });
