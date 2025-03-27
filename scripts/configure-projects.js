6da53c50ccb749810ea80a317ac02859aa0d3369
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2025-03-27 10:17:51 +0100

    not Windows fix

--- scripts/configure-projects.js
+++ scripts/configure-projects.js
@@ -26,25 +26,16 @@ const {
  *
  * @note A naive search on disk might yield false positives so we also try to
  * use the stack trace to find it. This currently works in Node (V8) and Bun
- * (JSC), but not on Windows for some reason. On Windows, this causes 'xpath' to
- * crash:
+ * (JSC).
  *
- *     [xmldom error]	invalid doc source
- *     #[line:0,col:undefined]
- *
- *     D:\~\node_modules\xpath\xpath.js:1787
- *         if (firstNode.nodeType === 9) {
- *                       ^
- *     TypeError: Cannot read properties of undefined (reading 'nodeType')
- *
- * @returns {string}
+ * @returns {string} Path to `react-native.config.[ts,mjs,cjs,js]`
  */
 function findReactNativeConfig(fs = nodefs) {
   // stack[0] holds this file
   // stack[1] holds where this function was called
   // stack[2] holds the file we're interested in
   const position = 2;
-  if (position < Error.stackTraceLimit && process.platform !== "win32") {
+  if (position < Error.stackTraceLimit) {
     const orig_prepareStackTrace = Error.prepareStackTrace;
     let stack;
     try {
@@ -56,7 +47,11 @@ function findReactNativeConfig(fs = nodefs) {
 
     if (Array.isArray(stack)) {
       const callsite = stack[position];
-      if (callsite && typeof callsite === "object" && "getFileName" in callsite) {
+      if (
+        callsite &&
+        typeof callsite === "object" &&
+        "getFileName" in callsite
+      ) {
         const file = callsite.getFileName();
         if (path.basename(file).startsWith("react-native.config.")) {
           return file;
