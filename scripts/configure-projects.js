bf8abbcb49765f5e7bf18cea2005465dcfb3070d
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2025-03-27 09:24:44 +0100

    fix: support more extensions when looking for `react-native.config.*`

--- scripts/configure-projects.js
+++ scripts/configure-projects.js
@@ -1,6 +1,8 @@
 // @ts-check
 "use strict";
 
+/** @import { ProjectConfig, ProjectParams } from "./types.js"; */
+
 /**
  * This script (and its dependencies) currently cannot be converted to ESM
  * because it is consumed in `react-native.config.js`.
@@ -19,7 +21,54 @@ const {
   v,
 } = require("./helpers");
 
-/** @import { ProjectConfig, ProjectParams } from "./types.js"; */
+/**
+ * Finds `react-native.config.[ts,mjs,cjs,js]`.
+ *
+ * @note A naive search on disk might yield false positives so we also try to
+ * use the stack trace to find it. This currently works in Node (V8) and Bun
+ * (JSC).
+ *
+ * @returns {string}
+ */
+function findReactNativeConfig(fs = nodefs) {
+  // stack[0] holds this file
+  // stack[1] holds where this function was called
+  // stack[2] holds the file we're interested in
+  const position = 2;
+  if (position < Error.stackTraceLimit) {
+    const orig_prepareStackTrace = Error.prepareStackTrace;
+    let stack;
+    try {
+      Error.prepareStackTrace = (_, stack) => stack;
+      stack = new Error().stack;
+    } finally {
+      Error.prepareStackTrace = orig_prepareStackTrace;
+    }
+
+    if (Array.isArray(stack)) {
+      const file = stack[position]?.getFileName();
+      if (file) {
+        return file;
+      }
+    }
+  }
+
+  const configFiles = [
+    "react-native.config.ts",
+    "react-native.config.mjs",
+    "react-native.config.cjs",
+    "react-native.config.js",
+  ];
+
+  for (const file of configFiles) {
+    const reactNativeConfig = findNearest(file, undefined, fs);
+    if (reactNativeConfig) {
+      return reactNativeConfig;
+    }
+  }
+
+  throw new Error("Failed to find `react-native.config.[ts,mjs,cjs,js]`");
+}
 
 /**
  * Returns the version number of a React Native dependency.
@@ -94,14 +143,7 @@ function windowsProjectPath(solutionFile, fs = nodefs) {
  * @returns {Partial<ProjectParams>}
  */
 function configureProjects({ android, ios, windows }, fs = nodefs) {
-  const reactNativeConfig = findNearest(
-    "react-native.config.js",
-    undefined,
-    fs
-  );
-  if (!reactNativeConfig) {
-    throw new Error("Failed to find `react-native.config.js`");
-  }
+  const reactNativeConfig = findReactNativeConfig(fs);
 
   /** @type {Partial<ProjectParams>} */
   const config = {};
