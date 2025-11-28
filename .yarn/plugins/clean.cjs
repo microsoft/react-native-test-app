6713f28c57377153fdd891d988e58008207baa0b
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2025-11-28 16:05:01 +0000

    chore(yarn): make Yarn plugins support monorepos (#2617)

--- .yarn/plugins/clean.cjs
+++ .yarn/plugins/clean.cjs
@@ -1,9 +1,6 @@
 // @ts-check
-const { spawnSync } = require("node:child_process");
-const fs = require("node:fs");
-const path = require("node:path");
 
-/** @type {{ name: string; factory: (require: NodeRequire) => unknown; }} */
+/** @type {{ name: string; factory: (require: NodeJS.Require) => unknown; }} */
 module.exports = {
   name: "plugin-clean",
   factory: (require) => {
@@ -14,20 +11,35 @@ module.exports = {
       static paths = [["clean"]];
 
       async execute() {
-        const projectRoot = path.dirname(path.dirname(__dirname));
+        // @ts-expect-error Yarn internal package
+        const { Configuration, Project } = require("@yarnpkg/core");
+        // @ts-expect-error Yarn internal package
+        const { npath } = require("@yarnpkg/fslib");
+        const { spawnSync } = require("node:child_process");
+        const fs = require("node:fs");
 
-        // Remove the symlink first. On Windows, `git clean` resolves/traverses
-        // the symlink, causing an infinite loop.
-        const symlink = path.join(
-          projectRoot,
-          "example",
-          "node_modules",
-          "react-native-test-app"
+        const configuration = await Configuration.find(
+          this.context.cwd,
+          this.context.plugins
         );
-        fs.rmSync(symlink, { force: true, maxRetries: 3, recursive: true });
+        const { project } = await Project.find(configuration, this.context.cwd);
+
+        // Remove symlinks first. On Windows, `git clean` resolves/traverses
+        // symlinks, causing an infinite loop.
+        for (const ws of project.workspaces) {
+          const rntaPath = npath.join(
+            npath.fromPortablePath(ws.cwd),
+            "node_modules",
+            "react-native-test-app"
+          );
+          const stats = fs.lstatSync(rntaPath, { throwIfNoEntry: false });
+          if (stats?.isSymbolicLink()) {
+            fs.rmSync(rntaPath);
+          }
+        }
 
         spawnSync("git", ["clean", "-dfqx", "--exclude=.yarn/cache"], {
-          cwd: projectRoot,
+          cwd: npath.fromPortablePath(project.cwd),
           stdio: "inherit",
         });
       }
