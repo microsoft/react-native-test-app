f0259c7ed05d323c28a78a5928c65d76f8aef89f
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2026-08-03 15:55:14 +0200

    chore: replace `minimatch` with built-in `path.matchesGlob`

--- scripts/affected.ts
+++ scripts/affected.ts
@@ -1,7 +1,7 @@
 import { getBaseCommit, getChangedFiles, git } from "@rnx-kit/tools-git";
 import * as yaml from "js-yaml";
-import { Minimatch } from "minimatch";
 import * as fs from "node:fs";
+import * as path from "node:path";
 
 type MatchChangedFiles = { "any-glob-to-any-file": string[] };
 type Match = { "changed-files": MatchChangedFiles[] };
@@ -24,9 +24,8 @@ function loadLabels(): Record<string, Match[] | undefined> {
 /**
  * Makes platform specific file path matchers.
  */
-function makeMatchers(): Record<string, Minimatch[]> {
-  const matchers: Record<string, Minimatch[]> = {};
-  const options = { dot: true };
+function makeMatchers(): Record<string, string[]> {
+  const matchers: Record<string, string[]> = {};
   const labels = loadLabels();
 
   for (const [label, match] of Object.entries(labels)) {
@@ -36,7 +35,11 @@ function makeMatchers(): Record<string, Minimatch[]> {
 
     const patterns = match[0]["changed-files"][0]["any-glob-to-any-file"];
     const platform = label.split(": ")[1];
-    matchers[platform] = patterns.map((m) => new Minimatch(m, options));
+    matchers[platform] = patterns.map((pattern) =>
+      // Modify pattern to match filenames starting with a period (but ignore
+      // patterns that start with `*`)
+      pattern.replaceAll("/*", "/{.,}*")
+    );
   }
 
   return matchers;
@@ -54,15 +57,15 @@ function getAffectedPlatforms(targetBranch: string | undefined): string[] {
     return clean(Object.keys(platformMatchers));
   }
 
-  const changedFiles = getChangedFiles(baseCommit);
-  if (changedFiles.length === 0) {
+  const files = getChangedFiles(baseCommit);
+  if (files.length === 0) {
     // If there are no files, we are building default branch
     return clean(Object.keys(platformMatchers));
   }
 
   // All platforms are affected if `react-native` related packages are changed
   const lockfile = "yarn.lock";
-  if (changedFiles.includes(lockfile)) {
+  if (files.includes(lockfile)) {
     const diff = git("diff", baseCommit, lockfile);
     if (diff.includes("react-native")) {
       return clean(Object.keys(platformMatchers));
@@ -70,8 +73,8 @@ function getAffectedPlatforms(targetBranch: string | undefined): string[] {
   }
 
   const affectedPlatforms = new Set<string>();
-  for (const [platform, matchers] of Object.entries(platformMatchers)) {
-    if (matchers.some((m) => changedFiles.some((f) => m.match(f)))) {
+  for (const [platform, patterns] of Object.entries(platformMatchers)) {
+    if (patterns.some((m) => files.some((f) => path.matchesGlob(f, m)))) {
       affectedPlatforms.add(platform);
     }
   }
